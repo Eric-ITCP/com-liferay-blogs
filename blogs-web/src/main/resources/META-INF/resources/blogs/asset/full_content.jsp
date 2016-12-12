marcellustavares/liferay-portal

LPS-69400 update references for Blogs

modules/apps/collaboration/blogs/blogs-web/src/main/resources/META-INF/resources/blogs/asset/full_content.jsp
@@ -55,13 +55,13 @@ Portlet portlet = PortletLocalServiceUtil.getPortletById(company.getCompanyId(),
 
 		<%= entry.getContent() %>
 
-		<liferay-ui:custom-attributes-available className="<%= BlogsEntry.class.getName() %>">
-			<liferay-ui:custom-attribute-list
+		<liferay-custom-field-ui:custom-attribute-available className="<%= BlogsEntry.class.getName() %>">
+			<liferay-custom-field-ui:custom-attribute-list
 				className="<%= BlogsEntry.class.getName() %>"
 				classPK="<%= (entry != null) ? entry.getEntryId() : 0 %>"
 				editable="<%= false %>"
 				label="<%= true %>"
 			/>
-		</liferay-ui:custom-attributes-available>
+		</liferay-custom-field-ui:custom-attribute-available>
 	</div>
 </div>

modules/apps/collaboration/blogs/blogs-web/src/main/resources/META-INF/resources/blogs/edit_entry.jsp
@@ -279,16 +279,16 @@ if (portletTitleBasedNavigation) {
 					</c:if>
 				</aui:fieldset>
 
-				<liferay-ui:custom-attributes-available className="<%= BlogsEntry.class.getName() %>">
+				<liferay-custom-field-ui:custom-attribute-available className="<%= BlogsEntry.class.getName() %>">
 					<aui:fieldset collapsed="<%= true %>" collapsible="<%= true %>" label="custom-fields">
-						<liferay-ui:custom-attribute-list
+						<liferay-custom-field-ui:custom-attribute-list
 							className="<%= BlogsEntry.class.getName() %>"
 							classPK="<%= entryId %>"
 							editable="<%= true %>"
 							label="<%= true %>"
 						/>
 					</aui:fieldset>
-				</liferay-ui:custom-attributes-available>
+				</liferay-custom-field-ui:custom-attribute-available>
 
 				<c:if test="<%= (entry == null) || (entry.getStatus() == WorkflowConstants.STATUS_DRAFT) %>">
 					<aui:fieldset collapsed="<%= true %>" collapsible="<%= true %>" label="permissions">

modules/apps/collaboration/blogs/blogs-web/src/main/resources/META-INF/resources/blogs/view_entry_content.jsp
@@ -167,14 +167,14 @@ String socialBookmarksDisplayPosition = blogsPortletInstanceConfiguration.social
 							<%= entry.getContent() %>
 						</div>
 
-						<liferay-ui:custom-attributes-available className="<%= BlogsEntry.class.getName() %>">
-							<liferay-ui:custom-attribute-list
+						<liferay-custom-field-ui:custom-attribute-available className="<%= BlogsEntry.class.getName() %>">
+							<liferay-custom-field-ui:custom-attribute-list
 								className="<%= BlogsEntry.class.getName() %>"
 								classPK="<%= entry.getEntryId() %>"
 								editable="<%= false %>"
 								label="<%= true %>"
 							/>
-						</liferay-ui:custom-attributes-available>
+						</liferay-custom-field-ui:custom-attribute-available>
 					</c:when>
 				</c:choose>
 			</div>

Eric-ITCP/com-liferay-blogs

blogs-web/src/main/resources/META-INF/resources/blogs/asset/full_content.jsp
@@ -55,13 +55,13 @@ Portlet portlet = PortletLocalServiceUtil.getPortletById(company.getCompanyId(),
 
 		<%= entry.getContent() %>
 
-		<liferay-ui:custom-attributes-available className="<%= BlogsEntry.class.getName() %>">
-			<liferay-ui:custom-attribute-list
+		<liferay-custom-field-ui:custom-attributes-available className="<%= BlogsEntry.class.getName() %>">
+			<liferay-custom-field-ui:custom-attribute-list
 				className="<%= BlogsEntry.class.getName() %>"
 				classPK="<%= (entry != null) ? entry.getEntryId() : 0 %>"
 				editable="<%= false %>"
 				label="<%= true %>"
 			/>
-		</liferay-ui:custom-attributes-available>
+		</liferay-custom-field-ui:custom-attributes-available>
 	</div>
 </div>

blogs-web/src/main/resources/META-INF/resources/blogs/edit_entry.jsp
@@ -305,16 +305,16 @@ if (portletTitleBasedNavigation) {
 					</c:if>
 				</aui:fieldset>
 
-				<liferay-ui:custom-attributes-available className="<%= BlogsEntry.class.getName() %>">
+				<liferay-custom-field-ui:custom-attributes-available className="<%= BlogsEntry.class.getName() %>">
 					<aui:fieldset collapsed="<%= true %>" collapsible="<%= true %>" label="custom-fields">
-						<liferay-ui:custom-attribute-list
+						<liferay-custom-field-ui:custom-attribute-list
 							className="<%= BlogsEntry.class.getName() %>"
 							classPK="<%= entryId %>"
 							editable="<%= true %>"
 							label="<%= true %>"
 						/>
 					</aui:fieldset>
-				</liferay-ui:custom-attributes-available>
+				</liferay-custom-field-ui:custom-attributes-available>
 
 				<c:if test="<%= (entry == null) || (entry.getStatus() == WorkflowConstants.STATUS_DRAFT) %>">
 					<aui:fieldset collapsed="<%= true %>" collapsible="<%= true %>" label="permissions">

blogs-web/src/main/resources/META-INF/resources/blogs/view_entry_content.jsp
@@ -167,14 +167,14 @@ String socialBookmarksDisplayPosition = blogsPortletInstanceConfiguration.social
 							<%= entry.getContent() %>
 						</div>
 
-						<liferay-ui:custom-attributes-available className="<%= BlogsEntry.class.getName() %>">
-							<liferay-ui:custom-attribute-list
+						<liferay-custom-field-ui:custom-attributes-available className="<%= BlogsEntry.class.getName() %>">
+							<liferay-custom-field-ui:custom-attribute-list
 								className="<%= BlogsEntry.class.getName() %>"
 								classPK="<%= entry.getEntryId() %>"
 								editable="<%= false %>"
 								label="<%= true %>"
 							/>
-						</liferay-ui:custom-attributes-available>
+						</liferay-custom-field-ui:custom-attributes-available>
 					</c:when>
 				</c:choose>
 			</div>