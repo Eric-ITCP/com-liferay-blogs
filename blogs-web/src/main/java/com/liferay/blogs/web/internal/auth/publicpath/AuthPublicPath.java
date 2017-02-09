f2be5f91a9792c6a498a7ac23f3e0b66fb8b2ee5
Adolfo Pérez <adolfo.perez@liferay.com>
2017-02-09 09:14:48 +0100

    LPS-70397 Move into `auth.publicpath` package

--- blogs-web/src/main/java/com/liferay/blogs/web/internal/auth/publicpath/AuthPublicPath.java
+++ blogs-web/src/main/java/com/liferay/blogs/web/internal/auth/publicpath/AuthPublicPath.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.blogs.web.internal.portlet.path;
+package com.liferay.blogs.web.internal.auth.publicpath;
 
 import org.osgi.service.component.annotations.Component;
 
